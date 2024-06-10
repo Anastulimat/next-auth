9cc800880eae8d76ccf5a8514fb0ca6c17125c32
Anas TULIMAT <anas.tulimat@orange.com>
2024-06-10 23:14:48 +0200

    feat: add register form ui and actions server for it

--- components/auth/card-wrapper.tsx
+++ components/auth/card-wrapper.tsx
@@ -34,9 +34,17 @@ const CardWrapper = ({
             </CardContent>
 
             {showSocial &&
-                <CardFooter>
-                    <Social/>
-                </CardFooter>
+                <>
+                    <div className="relative mb-4 mx-8">
+                        <div className="absolute inset-0 flex items-center"><span className="w-full border-t"></span>
+                        </div>
+                        <div className="relative flex justify-center text-xs uppercase"><span
+                            className="bg-background px-2 text-muted-foreground">Or continue with</span></div>
+                    </div>
+                    <CardFooter>
+                        <Social/>
+                    </CardFooter>
+                </>
             }
 
             <CardFooter>
